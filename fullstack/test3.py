ddee48646511cc3844c69aa5387b4973521e5a4b
Tanasin Vivitvorn <tanasin@Tanasins-MacBook-Pro.local>
2019-02-25 00:07:49 +0700

    Add explaination for test3

--- fullstack/test3.py
+++ fullstack/test3.py
@@ -1,6 +1,6 @@
 import unittest
 import math
-from memory_profiler import profile
+#from memory_profiler import profile
 
 class Node: 
     def __init__(self, value):
@@ -8,7 +8,7 @@ class Node:
         self.parent = None
 
 
-@profile
+#@profile
 def setup():
     nodes = [11]
     for i in range(1, 10):
@@ -20,35 +20,37 @@ def setup():
     
     return nodes
 
-# @profile
-def find_path(node):
-    path = []
-    parent = node
-
-    while parent is not None:
-        path.append(parent.value)
-        parent = parent.parent
-
-    return path
-
-# @profile
+#This solution use for finds LCA in O(n) time
+#using single traversal of Binary Tree and without extra storage for path arrays.
+@profile
 def lca(node1, node2):
-    path1 = find_path(node1)
-    path2 = find_path(node2)
+    #if it has no parent then it is root node.
+    #it no needs to do anything more just return the root.
+    if node1.parent is None:
+        return node1.value
+    elif node2.parent is None:
+        return node2.value
 
-    size1 = len(path1)
-    size2 = len(path2)
+    #if they are equal then return any one of them
+    #because each of them is LCA itself.
+    if node1.value == node2.value:
+        return node1.value
 
-    index = size1 if size1 <= size2 else size2
-    while index > 0 and path1[size1 - index] != path2[size2 - index]:
-        index -= 1
-    
-    return path1[size1 - index]
+    #if they have same root then the root is LCA
+    #but if it is not then we need to tracking from lowest node.
+    #the lowest node is the node that has maximum value
+    #example node1.value > node2.value that means node1 is in the same or lower level than node2
+    if node1.parent.value == node2.parent.value:
+        return node1.parent.value
+    elif node1.value > node2.value:
+        return lca(node1.parent, node2)
+    elif node1.value < node2.value:
+        return lca(node1, node2.parent)
 
 nodes = setup()
-lca(nodes[8], nodes[5])
 print "LCA(6, 7) = ", lca(nodes[6], nodes[7])
 print "LCA(3, 7) = ", lca(nodes[3], nodes[7])
+print "LCA(8, 5) = ", lca(nodes[8], nodes[5])
 
 class TestNode(unittest.TestCase):
     def test(self):
